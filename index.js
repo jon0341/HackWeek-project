223ba742073542c099020f9ac4e04a4753fb31c1
Espinosa, Jonathan <JonathanEspinosa@rocketcentral.com>
2022-06-28 14:48:13 -0400

    added webserver

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-//access drivers
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+//app.post('/Employees', function(req, res) {
+
 const mysql = require("mysql");
 
 //config for database
@@ -10,16 +22,27 @@ database: "employees"
 })
 
 //connecting to database
-
 connection.connect(function(error) {
     if(error) {
         console.log("error");
         console.log(error);
     }
     console.log("connected!");
-    connection.query("SELECT * FROM employee_demographics", function(error, result, fields) {
+
+    //let id = req.body.id;
+    //console.log(id);
+
+    connection.query("SELECT * FROM employee_demographics WHERE empID=1001", function(error, result) {
         if(error)
         console.log(error);
-        console.table(result);
-    })
+
+    console.table(result);
+    
+    //res.send(result);
+    });
+});
+//});
+
+const webserver = app.listen(5000, function(){
+    console.log("node web server is running");
 });
